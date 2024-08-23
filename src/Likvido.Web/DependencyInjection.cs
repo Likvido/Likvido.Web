5c77723010f0bdeddce36acd1434da9cb30beea8
Søren Pedersen <sh.pedersen@gmail.com>
2024-08-23 10:17:32 +0200

    Register IIpAddressService as singleton

--- src/Likvido.Web/DependencyInjection.cs
+++ src/Likvido.Web/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using Likvido.Web.Services.IP;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Likvido.Web;
 
@@ -10,7 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddLikvidoWeb(this IServiceCollection services)
     {
         services.AddHttpContextAccessor();
-        services.AddScoped<IIpAddressService, IpAddressService>();
+        services.TryAddSingleton<IIpAddressService, IpAddressService>();
 
         return services;
     }
